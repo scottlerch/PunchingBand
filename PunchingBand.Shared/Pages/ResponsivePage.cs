fb6b3724f899ab5e18a2b941c088cbe179d47d89
Scott Lerch <scottlerch@gmail.com>
2015-06-06 11:35:45 -0700

    Add skeleton for new responsive home page

---
+++ PunchingBand.Shared/Pages/ResponsivePage.cs
@@ -0,0 +1,43 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using PunchingBand.Utilities;
+
+namespace PunchingBand.Pages
+{
+    public class ResponsivePage : Page
+    {
+        public ResponsivePage()
+        {
+            SizeChanged += ResponsivePage_SizeChanged;
+            Unloaded += ResponsivePage_Unloaded;
+        }
+
+        private void ResponsivePage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            SizeChanged -= ResponsivePage_SizeChanged;
+            Unloaded -= ResponsivePage_Unloaded;
+        }
+
+        private void ResponsivePage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var width = DeviceInfo.GetActualResolution(e.NewSize.Width);
+
+            if (width > 1170)
+            {
+                VisualStateManager.GoToState(this, "Large", true);
+            }
+            else if (width > 970)
+            {
+                VisualStateManager.GoToState(this, "Medium", true);
+            }
+            else if (width > 650)
+            {
+                VisualStateManager.GoToState(this, "Small", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "ExtraSmall", true);
+            }
+        }
+    }
+}
---
+++ PunchingBand.Shared/Utilities/DeviceInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Windows.Graphics.Display;
+
+namespace PunchingBand.Utilities
+{
+    public static class DeviceInfo
+    {
+        public static double GetActualResolution(double dimension)
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                return dimension;
+            }
+
+            double toReturn;
+            var scale = DisplayInformation.GetForCurrentView().ResolutionScale;
+
+            switch (scale)
+            {
+                case ResolutionScale.Scale100Percent:
+                    toReturn = dimension;
+                    break;
+                case ResolutionScale.Scale120Percent:
+                    toReturn = dimension * 1.2;
+                    break;
+                case ResolutionScale.Scale140Percent:
+                    toReturn = dimension * 1.4;
+                    break;
+                case ResolutionScale.Scale150Percent:
+                    toReturn = dimension * 1.5;
+                    break;
+                case ResolutionScale.Scale160Percent:
+                    toReturn = dimension * 1.6;
+                    break;
+                case ResolutionScale.Scale180Percent:
+                    toReturn = dimension * 1.8;
+                    break;
+                case ResolutionScale.Scale225Percent:
+                    toReturn = dimension * 2.25;
+                    break;
+                default:
+                    toReturn = dimension;
+                    break;
+            }
+
+            return Math.Round(toReturn);
+        }
+    }
+}
