2daed563924faa98c2d91067d6a84d98967d0dc3
Scott Lerch <scottlerch@gmail.com>
2015-07-12 12:30:15 -0700

    Add missing file

---
+++ PunchingBand.Shared/Models/Enums/FistSides.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PunchingBand.Models.Enums
+{
+    [Flags]
+    public enum FistSides
+    {
+        Unknown = 0,
+        Left = 1,
+        Right = 2,
+        LeftAndRight = Left | Right,
+    }
+}
