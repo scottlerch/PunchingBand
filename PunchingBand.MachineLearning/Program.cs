789216971cb8fa8c8253e9bbf032411ccbb10480
Scott Lerch <scottlerch@gmail.com>
2015-07-26 21:57:27 -0700

    Go back to only recognizing jab, hook, upper since body and cross aren't accurate

--- PunchingBand.MachineLearning/Program.cs
+++ PunchingBand.MachineLearning/Program.cs
@@ -14,87 +14,93 @@ namespace PunchingBand.MachineLearning
     {
         static void Main(string[] args)
         {
-            var fist = "Right";
+            var inputPath = @"C:\Users\Scott\Documents\GitHub\PunchingBand\punchdata\";
+            var outputPath = @"C:\Users\Scott\Documents\GitHub\PunchingBand\PunchingBand.Shared\Assets\NeuralNetworks\";
+            var excludedCategories = new[] { PunchType.Unknown, PunchType.Body, PunchType.Cross };
+            var sides = new[] { "Right", "Left" };
 
-            var rand = new Random(0);
-
-            var categories = ((PunchType[])Enum.GetValues(typeof(PunchType))).Except(new[] { PunchType.Unknown }).ToArray();
-            var categoryIndices = categories.ToDictionary(c => c.ToString(), c => (int)c);
-
-            var lines = File.ReadAllLines(@"C:\Users\Scott\Documents\GitHub\PunchingBand\punchdata\PunchVectors" + fist + ".csv").Skip(1);
-            var splitLines = lines.Select(line => line.Split(',')).Where(parts => categoryIndices.ContainsKey(parts[0])).OrderBy(x => rand.Next()).ToList();
-
-            double[][] inputs = splitLines.Select(line => line.Skip(1).Select(double.Parse).ToArray()).ToArray();
-            int[] classes = splitLines.Select(line => categoryIndices[line[0]]).ToArray();
-
-            // First we have to convert this problem into a way that  the neural
-            // network can handle. The first step is to expand the classes into 
-            // indicator vectors, where a 1 into a position signifies that this
-            // position indicates the class the sample belongs to.
-            double[][] outputs = Accord.Statistics.Tools.Expand(classes, -1, +1);
-
-            // Create an activation function for the net
-            var function = new BipolarSigmoidFunction();
-
-            // Create an activation network with the function and
-            var network = new ActivationNetwork(function, inputs[0].Length, (int)((inputs[0].Length + categories.Length) * 0.67), categories.Length);
-
-            // Randomly initialize the network
-            new NguyenWidrow(network).Randomize();
-
-            // Teach the network using parallel Rprop:
-            var teacher = new ParallelResilientBackpropagationLearning(network);
-
-            double error = 1.0;
-            const double desiredErrorLevel = 1e-5;
-            while (error > desiredErrorLevel)
-                error = teacher.RunEpoch(inputs, outputs);
-
-            Console.WriteLine("Model trained!");
-
-            // Checks if the network has learned
-            for (int i = 0; i < inputs.Length; i++)
+            foreach (var fist in sides)
             {
-                double[] answer = network.Compute(inputs[i]);
+                var rand = new Random(0);
 
-                int expected = classes[i];
-                int actual; answer.Max(out actual);
+                var categories = ((PunchType[])Enum.GetValues(typeof(PunchType))).Except(excludedCategories).ToArray();
+                var categoryIndices = categories.ToDictionary(c => c.ToString(), c => (int)c);
 
-                // actual should be equal to expected
-                Console.WriteLine("Expected: {0}  Actual: {1}", expected, actual);
-            }
+                var lines = File.ReadAllLines(inputPath + "PunchVectors" + fist + ".csv").Skip(1);
+                var splitLines = lines.Select(line => line.Split(',')).Where(parts => categoryIndices.ContainsKey(parts[0])).OrderBy(x => rand.Next()).ToList();
 
-            //network.Save(@"C:\Users\Scott\Documents\GitHub\PunchingBand\punchdata\Punches" + fist + "Fist.dat");
+                double[][] inputs = splitLines.Select(line => line.Skip(1).Select(double.Parse).ToArray()).ToArray();
+                int[] classes = splitLines.Select(line => categoryIndices[line[0]]).ToArray();
 
-            var networkJson = JsonConvert.SerializeObject(network);
+                // First we have to convert this problem into a way that  the neural
+                // network can handle. The first step is to expand the classes into 
+                // indicator vectors, where a 1 into a position signifies that this
+                // position indicates the class the sample belongs to.
+                double[][] outputs = Accord.Statistics.Tools.Expand(classes, -1, +1);
 
-            File.WriteAllText(@"C:\Users\Scott\Documents\GitHub\PunchingBand\punchdata\Punches" + fist + "Fist.json", networkJson);
+                // Create an activation function for the net
+                var function = new BipolarSigmoidFunction();
 
-            Console.WriteLine("Model written to disk!");
+                // Create an activation network with the function and
+                var network = new ActivationNetwork(function, inputs[0].Length, (int)((inputs[0].Length + categories.Length) * 0.67), categories.Length);
 
-            Console.WriteLine("Verifying model on disk...");
+                // Randomly initialize the network
+                new NguyenWidrow(network).Randomize();
 
-            var jsonNetwork = new JsonNetwork();
+                // Teach the network using parallel Rprop:
+                var teacher = new ParallelResilientBackpropagationLearning(network);
 
-            using (var file = File.OpenRead(@"C:\Users\Scott\Documents\GitHub\PunchingBand\punchdata\Punches" + fist + "Fist.json"))
-            using (var streamReader = new StreamReader(file))
-            {
-                var serializer = new JsonSerializer();
-                serializer.Populate(streamReader, jsonNetwork);
-            }
+                double error = 1.0;
+                const double desiredErrorLevel = 1e-5;
+                while (error > desiredErrorLevel)
+                    error = teacher.RunEpoch(inputs, outputs);
 
-            var newNetwork = jsonNetwork.CreateActivationNetwork();
+                Console.WriteLine("Model trained!");
 
-            // Checks if the network has learned
-            for (int i = 0; i < inputs.Length; i++)
-            {
-                double[] answer = newNetwork.Compute(inputs[i]);
+                // Checks if the network has learned
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    double[] answer = network.Compute(inputs[i]);
 
-                int expected = classes[i];
-                int actual; answer.Max(out actual);
+                    int expected = classes[i];
+                    int actual; answer.Max(out actual);
 
-                // actual should be equal to expected
-                Console.WriteLine("Expected: {0}  Actual: {1}", expected, actual);
+                    // actual should be equal to expected
+                    Console.WriteLine("Expected: {0}  Actual: {1}", expected, actual);
+                }
+
+                //network.Save(rootPath + "Punches" + fist + "Fist.dat");
+
+                var networkJson = JsonConvert.SerializeObject(network);
+
+                File.WriteAllText(outputPath + "Punches" + fist + "Fist.json", networkJson);
+
+                Console.WriteLine("Model written to disk!");
+
+                Console.WriteLine("Verifying model on disk...");
+
+                var jsonNetwork = new JsonNetwork();
+
+                using (var file = File.OpenRead(outputPath + "Punches" + fist + "Fist.json"))
+                using (var streamReader = new StreamReader(file))
+                {
+                    var serializer = new JsonSerializer();
+                    serializer.Populate(streamReader, jsonNetwork);
+                }
+
+                var newNetwork = jsonNetwork.CreateActivationNetwork();
+
+                // Checks if the network has learned
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    double[] answer = newNetwork.Compute(inputs[i]);
+
+                    int expected = classes[i];
+                    int actual; answer.Max(out actual);
+
+                    // actual should be equal to expected
+                    Console.WriteLine("Expected: {0}  Actual: {1}", expected, actual);
+                }
             }
 
             Console.WriteLine("Done!");
